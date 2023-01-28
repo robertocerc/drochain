f9ed0200ed7ffb98bf3e6fe2f28eafeeed1d12a9
robertocerc <50298421+robertocerc@users.noreply.github.com>
2023-01-28 19:18:28 +0100

    Update DocSection.vue

--- src/components/DocSection.vue
+++ src/components/DocSection.vue
@@ -16,12 +16,6 @@
                                     scope="col"
                                     class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
                                 >
-                                    Title
-                                </th> 
-                                <th
-                                    scope="col"
-                                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                                >
                                     Hash
                                 </th>
                                 <th

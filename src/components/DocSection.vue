2dd7a327961f23e6254d1a97498a9dfba279d115
robertocerc <50298421+robertocerc@users.noreply.github.com>
2023-01-28 19:17:07 +0100

    Update DocSection.vue

--- src/components/DocSection.vue
+++ src/components/DocSection.vue
@@ -17,7 +17,8 @@
                                     class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
                                 >
                                     Title
-                                </th> <th
+                                </th> 
+                                <th
                                     scope="col"
                                     class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
                                 >
